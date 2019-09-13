87fd01d0ae5ab9338bdd66b763409ca634cb96a8
Jakub Krchlik <j.krchlik@gmail.com>
2019-09-13 11:05:32 +0200

    cleaning
    hopefully

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,15 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-  module.exports = [
-    {
-      "id": "cordova-sqlite-storage.SQLitePlugin",
-      "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
-      "pluginId": "cordova-sqlite-storage",
-      "clobbers": [
-        "SQLitePlugin"
-      ]
-    }
-  ];
+  module.exports = [];
   module.exports.metadata = {
-    "cordova-sqlite-storage": "3.3.0"
+    "cordova-plugin-whitelist": "1.3.4"
   };
 });
