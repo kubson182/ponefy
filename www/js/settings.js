ade75c7323b49226948ee87f26fb76763d028fe9
Jakub Krchlik <j.krchlik@gmail.com>
2019-09-17 20:44:37 +0200

    dark theme setting working
    on all pages

--- www/js/settings.js
+++ www/js/settings.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
         var settings = JSON.parse(localStorage.getItem("settings"));
 
         if(settings === null){
+            //initial settings
             settings = [["invert","darktheme"]];
             var invert = 0;
             var darktheme = 0;
@@ -19,78 +20,43 @@ $(document).ready(function () {
             var darktheme = settings[1][1];
             console.log("invert setting: " + invert +", darktheme: "+ darktheme);
 
-            //check if darktheme is selected, if yes, set the switch to checked
-            if (darktheme == 1){
-                console.log("mal by byt selectnuty darktheme switch");
-                $("#darkthemeswitch").prop("checked",true);
-                $("#style").attr("href","css/style-dark.css");
-                //on click  turn off
-                $("#darkthemeswitch").on("click",function (){
-                    darkthemeoff();
-                });
+            // check the current settings and set the button
+            if(darktheme == 1){
+                $("#darkthemeswitch").addClass("checked");
+                $("#darkthemeswitcher").addClass("on");
+                darkthemeon();
             }else{
-                //on click  turn on
-                $("#darkthemeswitch").on("click",function (){
-                    darkthemeon();
-                    
-                });
+// do nothing
             }
 
 
+            
+           
 
-            if(invert == 1){
-                console.log("mal by byt selectnuty invert switch");
-                $("#invertswitch").prop("checked",true);
-                //on click  turn off
-                $("#invertswitch").on("click",function (){
+
+            $("#invertswitch").click(function(){
+                $("#invertswitch").toggleClass("checked");
+                $("#invertswitcher").toggleClass("on");
+                if($("#invertswitcher").hasClass("on") == false){
                     invertoff();
-                });
-            }else{
-                //on click  turn on
-                $("#invertswitch").on("click",function (){
+                }else{
                     inverton();
-                });
-            }
+                }
+            });
+
+
+            $("#darkthemeswitch").click(function(){
+                $("#darkthemeswitch").toggleClass("checked");
+                $("#darkthemeswitcher").toggleClass("on");
+                if($("#darkthemeswitcher").hasClass("on") == false){
+                    darkthemeoff();
+                }else{
+                    darkthemeon();
+                }
+            });
         }
 
-        //function to turn on the dark theme  
-        function darkthemeon(){
-            var darktheme = 1;
-            var setting = [invert,darktheme];
-            settings[1] = setting;
-            localStorage.setItem("settings", JSON.stringify(settings));
-            console.log("on");
-            console.log("invert setting: " + invert +", darktheme: "+ darktheme);
-            $("#style").attr("href","css/style-dark.css");
-        }
-
-        //function to turn off the dark theme
-        function darkthemeoff(){
-            var darktheme = 0;
-            var setting = [invert,darktheme];
-            settings[1] = setting;
-            localStorage.setItem("settings", JSON.stringify(settings));
-            console.log("off");
-            console.log("invert setting: " + invert +", darktheme: "+ darktheme);
-            $("#style").attr("href","css/style.css");
-        }
-
-        function inverton(){
-            var invert = 1;
-            var setting = [invert, darktheme];
-            settings[1] = setting;
-            localStorage.setItem("settings", JSON.stringify(settings));
-            console.log("on");
-            console.log("invert setting: " + invert +", darktheme: "+ darktheme);
-        }
-        function invertoff(){
-            var invert = 0;
-            var setting = [invert, darktheme];
-            settings[1] = setting;
-            localStorage.setItem("settings", JSON.stringify(settings));
-            console.log("off");
-            console.log("invert setting: " + invert +", darktheme: "+ darktheme);
-        }
+        
         
     
     } else {
@@ -102,3 +68,50 @@ $(document).ready(function () {
     
 
 });
+
+//function to turn on the dark theme  
+function darkthemeon(){
+    var settings = JSON.parse(localStorage.getItem("settings"));
+    var darktheme = 1;
+    var invert = settings[1][0];
+    var setting = [invert,darktheme];
+    settings[1] = setting;
+    localStorage.setItem("settings", JSON.stringify(settings));
+    console.log("on");
+    console.log("invert setting: " + invert +", darktheme: "+ darktheme);
+     $("#style").attr("href","css/style-dark.css");
+}
+
+//function to turn off the dark theme
+function darkthemeoff(){
+    var settings = JSON.parse(localStorage.getItem("settings"));
+    var darktheme = 0;
+    var invert = settings[1][0];
+    var setting = [invert,darktheme];
+    settings[1] = setting;
+    localStorage.setItem("settings", JSON.stringify(settings));
+    console.log("off");
+    console.log("invert setting: " + invert +", darktheme: "+ darktheme);
+    $("#style").attr("href","css/style.css");
+}
+
+function inverton(){
+    var settings = JSON.parse(localStorage.getItem("settings"));
+    var invert = 1;
+    var darktheme = settings[1][1];
+    var setting = [invert, darktheme];
+    settings[1] = setting;
+    localStorage.setItem("settings", JSON.stringify(settings));
+    console.log("on");
+    console.log("invert setting: " + invert +", darktheme: "+ darktheme);
+}
+function invertoff(){
+    var settings = JSON.parse(localStorage.getItem("settings"));
+    var invert = 0;
+    var darktheme = settings[1][1];
+    var setting = [invert, darktheme];
+    settings[1] = setting;
+    localStorage.setItem("settings", JSON.stringify(settings));
+    console.log("off");
+    console.log("invert setting: " + invert +", darktheme: "+ darktheme);
+}
